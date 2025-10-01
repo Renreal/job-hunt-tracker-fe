8bfb4fa225fe3b3a78d87f90b4a7e98aa7e6a5b4
Renreal <renreal.dev@gmail.com>
2025-10-01 21:52:04 +0800

    create landing page setup

---
+++ src/pages/Landing.tsx
@@ -0,0 +1,11 @@
+
+import '../styles/Landing.css'
+import LandingCover from '../assets/hero-bg.png'
+function Landing() {
+  return (
+    <div className="landing-page">
+      <div className="content">landing</div>
+    </div>
+  )
+}
+export default Landing;
