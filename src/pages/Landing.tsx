8519410a8b203f7c637d32f280fde45178a934ae
Renreal <renreal.dev@gmail.com>
2025-10-04 19:16:34 +0800

    update landing page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,10 +1,20 @@
 
 import '../styles/Landing.css'
-import LandingCover from '../assets/hero-bg.png'
+import Logo from "../assets/logo.png"
 function Landing() {
   return (
     <div className="landing-page">
-      <div className="content">landing</div>
+      <div className="login-signup-bar">
+        <span className='login'>LOG IN</span>
+        <span className='sign-up'>SIGN UP</span>
+      </div>
+      <div className='hero-image-container'>
+      </div>
+      <div className="content">
+        <img src={Logo} alt="" />
+        <h1>ORGANIZE. TRACK. GET HIRED.</h1>
+        <h3>Stay on top of your job search with Job Hunt Tracker  your free tool to manage applications, monitor progress, and land your next opportunity with confidenc</h3>
+      </div>
     </div>
   )
 }
