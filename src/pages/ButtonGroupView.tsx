0db4864ac9d5b1b768651f4819e24a0622f5d278
Renreal <renreal.dev@gmail.com>
2025-11-04 20:20:01 +0800

    fix typo

--- src/pages/ButtonGroupView.tsx
+++ src/pages/ButtonGroupView.tsx
@@ -179,7 +179,7 @@ export function ButtonGroupView({ user }: { user: UserData }) {
                   <SelectValue placeholder="Select status" />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value="interviews">Interview</SelectItem>
+                  <SelectItem value="interview">Interview</SelectItem>
                   <SelectItem value="pending">Pending</SelectItem>
                   <SelectItem value="job offer">Job Offer</SelectItem>
                 </SelectContent>
